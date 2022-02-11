2a5e3883dd667ccbfefe4b9ceebd53a560f53569
zeeshan khan <zeeshan@fastapp.in>
2022-02-11 15:24:17 +0530

    made changes

--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
-const token = '$2b$10$RDewmUZ.M0bvVvGtyArZdOlW2ZapDvh8SAHR7y4A/dZgUrYECnbYy';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'https://my-json-server.typicode.com/Zeeshankhan085/json-ads',
 });
 
 export default instance;
